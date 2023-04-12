9a6bceb166c73e8e5231dc306e04e122cb03d950
Francis Buabin Owusu <codingfrancis100@gmail.com>
2023-04-12 17:47:11 +0000

    fixed router issues

--- src/routes/authentication.ts
+++ src/routes/authentication.ts
@@ -1,10 +1,5 @@
 import express, {Response, Request} from 'express'
 
-const controller = (req: Request, res: Response) => {
-    res.send('Hello')
-}
-
 export = (router: any) => {
-    console.log('mainiii')
-    router.get('/user', controller)
+    router.get('/user', )
 }
